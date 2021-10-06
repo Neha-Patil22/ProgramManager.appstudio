4ab6ac9df254bc9348f4761f6363741c9b5633a7
Neha-Patil22 <nbp30352@creighton.edu>
2021-10-06 14:50:48 -0500

    v11

--- forms/products/products.js
+++ forms/products/products.js
@@ -2,7 +2,7 @@
 the user to add to the list or can delete the last item on the list. The program also shows the user the array contents 
 after a new product is added. */
 
-
+/*
 let storeProducts=[ 'milk', 'cheese', 'cream', 'eggs']
 
 let product = prompt("Do wants to add a new product or delete the last product? reply with add or delete" )
@@ -20,5 +20,5 @@ if (product == 'add'){
 for (let i = 0; i < storeProducts.length; i++) 
  console.log(`${storeProducts[i]}`)
 
-
+*/
 
